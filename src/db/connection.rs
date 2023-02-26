405b14149e707155083f815209072c77977f6809
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-02-27 02:02:46 +0300

    Limit number of db connections to 4

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -9,6 +9,7 @@ pub fn establish_pooled_connection() -> PooledConnection<ConnectionManager<Mysql
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     Pool::builder()
+        .max_size(4)
         .build(ConnectionManager::<MysqlConnection>::new(&database_url))
         .expect("Failed to create pool.")
         .clone()
--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,6 @@ async fn main() -> std::io::Result<()> {
     let data = web::Data::new(AppState::new(establish_pooled_connection()));
 
     HttpServer::new(move || {
-        log::info!("NewWorker");
         let cors = Cors::default()
             .allowed_origin("http://127.0.0.1:8081")
             .allowed_origin("http://localhost:8081")
@@ -117,7 +116,6 @@ async fn main() -> std::io::Result<()> {
             .service(home)
             .service(actix_files::Files::new("/", "./assets").show_files_listing())
     })
-    .workers(1)
     .bind(("0.0.0.0", port))?
     .run()
     .await
