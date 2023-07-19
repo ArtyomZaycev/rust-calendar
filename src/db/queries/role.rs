0d08cd1133125c72da76e8a9857c262275dd69fc
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-07-20 00:14:37 +0300

    Cleanup

--- src/db/queries/role.rs
+++ src/db/queries/role.rs
@@ -17,19 +17,6 @@ pub fn db_load_roles_by_user_id(
         .map_err(|e| Error::DieselError(e))
 }
 
-pub fn db_load_role_by_id(
-    connection: &mut MysqlConnection,
-    rid: i32,
-) -> Result<Option<DbRole>, Error> {
-    use crate::db::schema::roles::dsl::*;
-
-    roles
-        .find(rid)
-        .load::<DbRole>(connection)
-        .map(|v| v.into_iter().nth(0))
-        .map_err(|e| Error::DieselError(e))
-}
-
 pub fn db_load_roles(connection: &mut MysqlConnection) -> Result<Vec<DbRole>, Error> {
     use crate::db::schema::roles::dsl::*;
 
