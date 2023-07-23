7e72e957d66aef3224231cd52455b2c856f5e240
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-07-23 12:22:15 +0300

    Fix migration

--- migrations/2023-07-23-091355_demo_account/up.sql
+++ migrations/2023-07-23-091355_demo_account/up.sql
@@ -2,4 +2,4 @@ INSERT INTO `users` (`id`, `name`, `email`)
 VALUES(2, "demo", "demo@aspid.xyz");
 
 INSERT INTO `passwords`(`user_id`, `name`, `password`, `access_level`, `edit_right`)
-VALUES(2, "Full", "1", 1000, true);
+VALUES(2, "Full", SHA2("1", 512), 1000, true);
